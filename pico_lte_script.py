c203ccede8587796a0673395826dbbd869e2269a
Caden <cvfxz16@gmail.com>
2024-12-05 20:29:18 -0600

    Add files via upload

---
+++ pico_lte_script.py
@@ -0,0 +1,56 @@
+import machine
+from machine import Pin, Timer
+import neopixel
+import utime
+import os
+import time
+from pico_lte.utils.status import Status
+from pico_lte.core import PicoLTE
+from pico_lte.common import debug
+
+# Set configurations
+debug.set_level(0)
+
+# Definitions and signatures
+USER_LED = Pin(22, mode=Pin.OUT)
+NEOPIXEL_NUM_LEDS = 8
+NEOPIXEL_PIN = machine.Pin(15)
+neopixel = neopixel.NeoPixel(NEOPIXEL_PIN, NEOPIXEL_NUM_LEDS)
+
+def set_neopixel_off():
+    for i in range(NEOPIXEL_NUM_LEDS):
+        neopixel[i] = (0, 0, 0)
+    neopixel.write()
+    
+# Set NeoPixel LEDs and user LED off
+set_neopixel_off();
+USER_LED.off()
+
+# Initialize file system as needed
+print("\n**** Reading root directory... ****")
+print(os.listdir())
+f = open("data.txt", "a")
+data = f.read()
+print("\n**** Data file content:")
+if not data:
+    print("NO DATA FOUND")
+else:
+    print(data)
+f.close()
+
+# Log ambient temperature for fun
+print("\n**** Reading ambient temperature... ****")
+sensor_temp = machine.ADC(4)
+conversion_factor = 3.3 / (65535)
+reading = sensor_temp.read_u16() * conversion_factor
+temp = 27 - (reading -0.706)/(0.001721)
+print(temp)
+
+# Reach out to web for data
+print("\n**** Registering LTE network... ****")
+picoLTE = PicoLTE()
+
+print("\n**** Sending a request to read table... ****")
+result = picoLTE.google_sheets.get_data(sheet="Sheet1", data_range="A1:C2")
+debug.info("Result:", result)
+
